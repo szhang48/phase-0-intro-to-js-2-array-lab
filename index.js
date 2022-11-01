a83932b2df24c8cf87c64fd6f5113b09b9f651ef
Sandy Zhang <sandyzhang142@gmail.com>
2022-11-01 13:45:38 -0400

    commit tree fe4686815cfe37105b4aece2b3171bc743363b67

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]; 
+
+function destructivelyAppendCat(cat) {
+    return cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) { 
+    return cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+function appendCat(name) { 
+    const cat = [...cats, name];
+    return cat;
+}
+function prependCat(name) { 
+    const cat = [name, ...cats];
+    return cat;
+}
+function removeLastCat() {
+    const cat = cats.slice(0, -1);
+    return cat;
+
+}
+function removeFirstCat() {
+    const cat = cats.slice(1); 
+    return cat;
+}
